260452bee9874f0661a405669e75b15230cfdcd7
Lenovo-User <Lenovo-User@DESKTOP-66DCH94>
2024-01-19 15:31:56 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim myitem
+
+        myitem = TextBox1.Text
+        ListBox1.Items.Add(myitem)
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Me.Label2.Text = ListBox1.SelectedItem.ToString
+
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        Me.Label2.ForeColor = System.Drawing.Color.Purple
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+        Me.Label2.ForeColor = System.Drawing.Color.Blue
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        Label2.Text = Label2.Text.ToUpper
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        Label2.Text = Label2.Text.ToLower
+    End Sub
+End Class
